44f6548f0f7aac7b4c0beaa56c91e4fcea9f5ac2
阿航 <wyj234673@alibaba-inc.com>
2026-04-27 16:34:20 +0800

    chore: 将 dist/ 加入 .gitignore，从 Git 跟踪中移除构建产物

--- dist/server/server/services/import/index.js
+++
@@ -1,4 +0,0 @@
-// Unified re-exports for the import service module
-export { parseGitHubUrl, parseGiteeUrl, parseGitLabUrl, parseClawHubUrl, parseBitbucketUrl, } from './urlParsers.js';
-export { githubApiFetch, getGitHubRepoInfo, downloadRepoAsZip, giteeApiFetch, getGiteeRepoInfo, gitlabApiFetch, getGitLabRepoInfo, bitbucketApiFetch, getBitbucketRepoInfo, downloadBitbucketContents, } from './gitApis.js';
-export { registerImportProvider, getImportProviders, detectProvider, } from './providerRegistry.js';
